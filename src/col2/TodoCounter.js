258acb6a63cb716ea31e68cbcc1de4c62966defa
Julio Ribera <julio@magiaycardistry.com>
2023-11-21 20:26:41 +0100

    Micro cambio absurdo

--- src/col2/TodoCounter.js
+++ src/col2/TodoCounter.js
@@ -1,7 +1,8 @@
 function TodoCounter({total, completed}){
   
     return (
-      total === 0 ? <h4>Crea una nueva tarea</h4> : <h4><b>{completed}</b> de <b>{total}</b> completadas</h4>
+      total === 0 ? <h4>Crea una nueva tarea</h4>
+      : <h4><b>{completed}</b> de <b>{total}</b> completadas</h4>
     );
   } 
 
